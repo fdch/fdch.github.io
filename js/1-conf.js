660af2e490c513b1efd73a66985d732b827a03f3
Fede Camara Halac <camarafede@gmail.com>
2019-06-25 12:11:18 +0200

    tidying up globals

--- js/1-conf.js
+++ js/1-conf.js
@@ -1,30 +1,11 @@
 var latest_bio_pic = "bio_vienna.jpg";
 var latest_bkg_pic = ["orkna", "orkna-1.png"];
 
-var htmlTag, bodyTag, headerTag, mainTag, footerTag;
-var h1titlTag, navigaTag, iframeTag, rotImgTag, 
-navSelTag, imgDivTag, catDivTag,catSelTag;
-var mainBack;
-var allUnwork={}, allPapers={}, allEvents={}, allPeople={};
-var allPerfor={}, allAwards={}, allCollab={}, allTeachi={};
-var allCategories=[], allTitles=[],allWorkId=[],uCategories=[];
-var allVideos=[], allAudios=[];
-
-var userLang;
-
 var mitem = [
-"init",
-"bio",
-"unwork",
-"papers",
-"events",
-"people",
-"touch",
-"cv",
-"games",
-// "audio",
-// "video"
+"init", "bio", "unwork", "papers", 
+"events", "people", "touch", "cv", "games",  // "audio", // "video"
 ];
+
 var currpage=0, loaded=0, loading=0;
 
 var w, h, t = 4333;
@@ -54,27 +35,6 @@ var instagram 	= "https://instagram.com/ffddcchh";
 var contactGif 	=  raw + imgpath + "zissou.gif";
 var loadingUrl 	=  raw + imgpath + "fdch.gif";
 
-var backImg 	= "url(\'"+loadingUrl+"\') center no-repeat;";
-var bioImage 	=  raw + imgpath + latest_bio_pic;
-var bkgImage 	=  raw + latest_bkg_pic[0]+ "/master/img/" + latest_bkg_pic[1];
-
-var rotImgStyle= [
-	"float:left",
-	"margin:2.5px 0 0 5px",
-	"clip-path:circle(50% at 50% 50%)",
-	"-webkit-clip-path:circle(40% at 50% 50%)"
-];
-var rotImg = [
-	raw+imgpath+"imgone-76.png",
-	raw+imgpath+"imgone-76-i.png",
-	rotImgStyle.join(";"),
-	"rotImgId",
-	"fdch~",
-	"imgClicker(this);"
-];
-
-var tilde = " ~ ";
-
 var color_preset = {
 	mid  : [55,200,100,20],
 	high : [30,200,50,100],
@@ -86,44 +46,40 @@ var subtitle 	= "Fede Cámara Halac";
 var unclink 	= "http://artes.unc.edu.ar";
 var nyulink 	= "http://gsas.nyu.edu";
 var unc 		= [
-					"Universidad Nacional de Córdoba",
-					"National University of Córdoba"
+"National University of Córdoba",
+"Universidad Nacional de Córdoba",
 ];
 var nyuTitle 	= [
-					"Music Composition and Theory",
-					"Composición y Teoría Musical"
+"Music Composition and Theory",
+"Composición y Teoría Musical"
 ];
 var uncTitle 	= [
-					"Licenciatura en Composición Musical",
-					"Licenciate in Music Composition"
+"Licenciate in Music Composition",
+"Licenciatura en Composición Musical",
 ];
 var nyu 		= [
-					"New York University",
-					"Universidad de Nueva York"
+"New York University",
+"Universidad de Nueva York"
 ];
 var advisors 	= [
-					"Jaime Oliver",
-					"Elizabeth Hoffman",
-					// "Martin Daughtry",
-					// "Robert Rowe",
-					// "William Brent"
+"Jaime Oliver",
+"Elizabeth Hoffman",
+// "Martin Daughtry",
+// "Robert Rowe",
+// "William Brent"
 ];
 var currently 	= ["PhD","PhD"];
 var tesis 		= [
-					"Database Music",
-					"Música con Base de Datos"
+"Database Music",
+"Música con Base de Datos"
 ];
 var bday = new Date(1988, 5, 8, 12, 1, 13, 128);
 
-var bioEnglish = subtitle+" studied at the "+uncTitle[0]+" at "+unc[0]+".\
- He has a "+currently[0]+" in "+nyuTitle[0]+" at "+nyu[0]+", where he studied with "+advisors.join(" and ")+".\
- His research focuses on "+tesis[0]+".\
- His work is available at "+url;
+var bioSpanish = 
+" Fede Cámara Halac investiga y hace sonidos, navega y diseña imágenes, y juega con bases de datos y multimedia. Obtuvo un PhD en Composición y Teoría Musical de la Universidad de Nueva York donde estudió con Jaime Oliver y Elizabeth Hoffman. Estudió Composición Musical en la Universidad Nacional de Córdoba. Su trabajo se encuentra disponible en https://fdch.github.io"
 
-var bioSpanish = subtitle+" estudió en la "+uncTitle[0]+" en "+unc[1]+".\
- Tiene un "+currently[1]+" de "+nyuTitle[1]+" en la "+nyu[1]+" donde estudió con "+advisors.join(" y ")+".\
- Su investigación se centra en la "+tesis[1]+".\
- Su trabajo se encuentra disponible en "+url;
+var bioEnglish = 
+"Fede Cámara Halac researches and makes sounds, navigates and designs images, and plays with databases and multimedia. He earned a PhD in Composition and Music Theory from New York University where he studied with Jaime Oliver and Elizabeth Hoffman. He studied Music Composition at the National University of Córdoba. His work is available at https://fdch.github.io"
 
 var personal = [
 "Full Name: Federico Nicolás Cámara Halac",
@@ -136,157 +92,6 @@ var personal = [
 "Graduate Education : "+ currently[0] + " in "+nyuTitle[0]+" at "+nyu[0]+" (2013-2019)",
 "Undergraduate : " +uncTitle[0] + " at the " +unc[0]+ " (2006-12)",
 ];
-
-var mylinks = {
-	"Instagram"	:instagram,
-	"SoundCloud":scloud,
-	"Github"	:repo,
-	"Vimeo"		:video,
-	"Youtube"	:yt,
-	"Tumblr"	:blog,
-	"Facebook"	:facebookUrl,
-	"Twitter"	:twitter,
-	"Flickr"	:flickr,
-	"LinkedIn"	:linkedin,
-}
-
-var vimeo = [
-"https://player.vimeo.com/video/",
-"?title=0&byline=0&portrait=0&likes=0&watchlater=0&share=0"
-];
-
-var youtube = [
-"https://www.youtube.com/embed/",
-"?rel=0&amp;controls=0&amp;showinfo=0"
-];
-
-var featURL = [
-youtube[0] + "hXONutQ_vYc" + youtube[1],
-youtube[0] + "GA0_ieFjpuM" + youtube[1],
-youtube[0] + "gErNVvwsOdI" + youtube[1],
-youtube[0] + "HpPNVJsn3XQ" + youtube[1],
-vimeo[0]   + "168692629"   + vimeo[1]  ,
-vimeo[0]   + "189384117"   + vimeo[1]  ,
-vimeo[0]   + "137084409"   + vimeo[1]  ,
-vimeo[0]   + "135750747"   + vimeo[1]  ,
-vimeo[0]   + "189537568"   + vimeo[1]  ,
-vimeo[0]   + "189548950"   + vimeo[1]  ,
-vimeo[0]   + "128082938"   + vimeo[1]  ,
-vimeo[0]   + "128763368"   + vimeo[1]  ,
-vimeo[0]   + "103288412"   + vimeo[1]  ,
-vimeo[0]   + "93202065"    + vimeo[1]  ,
-vimeo[0]   + "300566098"    + vimeo[1]   
-];
-
-var spreadsheets = "https://spreadsheets.google.com/feeds/list/";
-var sheetID = "1e5dh1sZX1QuFFioC-1ofcPIPhwU9i-lvUzOzn4_3SLQ/";
-var cvID = "1vViMWDsMRnbGUgP44XNDlQrLQ3MsdgO9W91mM4MxJtw/";
-var altjson = "/public/values?alt=json";
-
-var allGS = [
-	spreadsheets+sheetID+"1"+altjson,
-	spreadsheets+sheetID+"2"+altjson,
-	spreadsheets+sheetID+"3"+altjson,
-	spreadsheets+sheetID+"4"+altjson,
-	spreadsheets+cvID   +"1"+altjson,
-	spreadsheets+cvID   +"2"+altjson,
-	spreadsheets+cvID   +"3"+altjson,
-	spreadsheets+cvID   +"4"+altjson
-];
-
-
-var allCVsections = [
-	"Personal",
-	"Teachings",
-	"Awards",
-	"Unworks",
-	"Collaborations",
-	"Performances"
-];
-
-var webSections = [
-	"People",
-	"Events",
-	"Papers"
-]
-
-var formD = [articleWidth(maxWidth),height()];
-
-var formL = [
-	"https://docs.google.com/forms/d/e/1FAIpQLS",
-	"/viewform"
-]
-var formE = [
-	"<iframe src=\"https://docs.google.com/forms/d/e/1FAIpQLS",
-	"/viewform?embedded=true\" width=\""+formD[0]+"\" height=\""+formD[1]+"\" \
-	frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\">Loading...</iframe>"
-]
-var fonts = [
-"\'Courier\'",
-"\'Arial\'",
-"\'Helvetica\'",
-"\'Times New Roman\'",
-"\'Courier New\'",
-"\'Times\'",
-"\'Verdana\'",
-"\'Georgia\'",
-"\'Palatino\'"
-];
-
-var imgArray = [raw+imgpath+"imgone.png",raw+imgpath+"imgtwo.png"];
-var somestring='kontakte';
-var disableEnter = [
-	"<script>",
-	"$(document).keypress(",
-	"    function(event){",
-	"    if (event.which == '13') {",
-	"       event.preventDefault();",
-	"     }",
-	"});",
-	"</s" + "cript>",
-];
-
-
-var submit = [
-	"\"No, what is important is neither linearity or non-linearity, \
-	but the change, the degree of change from something that doesn't move \
-	to other events with different tempos in particular.\"",
-	"\"I no longer limit myself.\"",
-	"<head><meta name=\"viewport\" \
-	content=\"width=device-width, initial-scale=1\">\
-	<link rel=stylesheet href=\""+repo+"/css/style.css\"></style>"+disableEnter.join("")+"\
-	<link rel='shortcut icon' href=\""+raw+imgpath+"imgone.png\"></link>\
-	<title>Submit Form</title></head><body><h2>Submit Form</h2><div id=maindiv>\
-	<form><h3>Enter password:</h3>\
-	<input type=password id=krl size=12/><p>Click Submit when done</p>\
-	<input type=button value=Submit id=authOK /></form></div></body>",somestring,
-	"width=200, height=200, location=0, toolbar=0, resizable=0, scrollbars=0"
-];
-
-var forms = {
-	[allCVsections[1]]:"els-Lj6MXZOFfUvyxLHp8uhdkNzADgh3KGjWV0-CJPUiGnkA",//
-	[allCVsections[2]]:"cCNndRyaJTIqOI4KDnDxu3OSdLeVAMAAYI1p_D-ymTLi9Y7g",//
-	[allCVsections[3]]:"fVU3VMgZvkY3kGnG-OezP02MgqOvhwS6Z6APOflTE-ShvQVg",//
-	[allCVsections[4]]:"d51ZlszfuGuvMA9WXpY56mlottnn-26JZ_whsfaG-hX_uZFw",//
-	[allCVsections[5]]:"cr_d39dnsctc4jfCZ-Tn9g6MyUsXZB0wzOPssV80KITISLfA",//
-	[webSections[0]]  :"cnzdRLIMTy2arVYeIYOIJS_SPdedxngAhs8qbp8gnoJpIOhw",//
-	[webSections[1]]  :"cYgPrlbPXm3B9FDQghaaey8d5X6DpZhsOqMLP36DfrpRsOJA",//
-	[webSections[2]]  :"ftCkAv9TzkzlgAi8dcJNofLYYWc_k8WtSet3SQTf2ObOuyMA",//
-}
-var formLinks = {
-	[allCVsections[1]]: formL[0] + forms[allCVsections[1]] + formL[1],
-	[allCVsections[2]]: formL[0] + forms[allCVsections[2]] + formL[1],
-	[mitem[1]]        : formL[0] + forms[allCVsections[3]] + formL[1],
-	[allCVsections[4]]: formL[0] + forms[allCVsections[4]] + formL[1],
-	[allCVsections[5]]: formL[0] + forms[allCVsections[5]] + formL[1],
-	[mitem[4]]        : formL[0] + forms[webSections[0]]   + formL[1],
-	[mitem[3]]        : formL[0] + forms[webSections[1]]   + formL[1],
-	[mitem[2]]        : formL[0] + forms[webSections[2]]   + formL[1],
-	"games"           :"",
-	[mitem[5]]        :"",
-	[mitem[0]]        :""
-
-};
 
 var contactMessage = [
  "Send me an email at "+nyuid+" and I will get in touch with you\
---
+++ js/2-data.js
@@ -0,0 +1,183 @@
+
+var htmlTag, bodyTag, headerTag, mainTag, footerTag;
+var h1titlTag, navigaTag, iframeTag, rotImgTag; 
+var navSelTag, imgDivTag, catDivTag, catSelTag;
+var mainBack;
+var allUnwork={}, allPapers={}, allEvents={}, allPeople={};
+var allPerfor={}, allAwards={}, allCollab={}, allTeachi={};
+var allCategories=[], allTitles=[],allWorkId=[],uCategories=[];
+var allVideos=[], allAudios=[];
+
+var userLang;
+
+var backImg 	= "url(\'"+loadingUrl+"\') center no-repeat;";
+var bioImage 	=  raw + imgpath + latest_bio_pic;
+var bkgImage 	=  raw + latest_bkg_pic[0]+ "/master/img/" + latest_bkg_pic[1];
+
+var rotImgStyle= [
+	"float:left",
+	"margin:5px 0 0 5px",
+	"clip-path:circle(30% at 50% 50%)",
+	"-webkit-clip-path:circle(50% at 50% 50%)"
+];
+var rotImg = [
+	raw+imgpath+"imgone-76.png",
+	raw+imgpath+"imgone-76-i.png",
+	rotImgStyle.join(";"),
+	"rotImgId",
+	"fdch~",
+	"imgClicker(this);"
+];
+
+var tilde = " ~ ";
+
+var mylinks = {
+	"Instagram"	:instagram,
+	"SoundCloud":scloud,
+	"Github"	:repo,
+	"Vimeo"		:video,
+	"Youtube"	:yt,
+	"Tumblr"	:blog,
+	"Facebook"	:facebookUrl,
+	"Twitter"	:twitter,
+	"Flickr"	:flickr,
+	"LinkedIn"	:linkedin,
+}
+
+var vimeo = [
+"https://player.vimeo.com/video/",
+"?title=0&byline=0&portrait=0&likes=0&watchlater=0&share=0"
+];
+
+var youtube = [
+"https://www.youtube.com/embed/",
+"?rel=0&amp;controls=0&amp;showinfo=0"
+];
+
+var featURL = [
+youtube[0] + "hXONutQ_vYc" + youtube[1],
+youtube[0] + "GA0_ieFjpuM" + youtube[1],
+youtube[0] + "gErNVvwsOdI" + youtube[1],
+youtube[0] + "HpPNVJsn3XQ" + youtube[1],
+vimeo[0]   + "168692629"   + vimeo[1]  ,
+vimeo[0]   + "189384117"   + vimeo[1]  ,
+vimeo[0]   + "137084409"   + vimeo[1]  ,
+vimeo[0]   + "135750747"   + vimeo[1]  ,
+vimeo[0]   + "189537568"   + vimeo[1]  ,
+vimeo[0]   + "189548950"   + vimeo[1]  ,
+vimeo[0]   + "128082938"   + vimeo[1]  ,
+vimeo[0]   + "128763368"   + vimeo[1]  ,
+vimeo[0]   + "103288412"   + vimeo[1]  ,
+vimeo[0]   + "93202065"    + vimeo[1]  ,
+vimeo[0]   + "300566098"    + vimeo[1]   
+];
+
+var spreadsheets = "https://spreadsheets.google.com/feeds/list/";
+var sheetID = "1e5dh1sZX1QuFFioC-1ofcPIPhwU9i-lvUzOzn4_3SLQ/";
+var cvID = "1vViMWDsMRnbGUgP44XNDlQrLQ3MsdgO9W91mM4MxJtw/";
+var altjson = "/public/values?alt=json";
+
+var allGS = [
+	spreadsheets+sheetID+"1"+altjson,
+	spreadsheets+sheetID+"2"+altjson,
+	spreadsheets+sheetID+"3"+altjson,
+	spreadsheets+sheetID+"4"+altjson,
+	spreadsheets+cvID   +"1"+altjson,
+	spreadsheets+cvID   +"2"+altjson,
+	spreadsheets+cvID   +"3"+altjson,
+	spreadsheets+cvID   +"4"+altjson
+];
+
+
+var allCVsections = [
+	"Personal",
+	"Teachings",
+	"Awards",
+	"Unworks",
+	"Collaborations",
+	"Performances"
+];
+
+var webSections = [
+	"People",
+	"Events",
+	"Papers"
+]
+
+var formD = [articleWidth(maxWidth),height()];
+
+var formL = [
+	"https://docs.google.com/forms/d/e/1FAIpQLS",
+	"/viewform"
+]
+var formE = [
+	"<iframe src=\"https://docs.google.com/forms/d/e/1FAIpQLS",
+	"/viewform?embedded=true\" width=\""+formD[0]+"\" height=\""+formD[1]+"\" \
+	frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\">Loading...</iframe>"
+]
+var fonts = [
+"\'Courier\'",
+"\'Arial\'",
+"\'Helvetica\'",
+"\'Times New Roman\'",
+"\'Courier New\'",
+"\'Times\'",
+"\'Verdana\'",
+"\'Georgia\'",
+"\'Palatino\'"
+];
+
+var imgArray = [raw+imgpath+"imgone.png",raw+imgpath+"imgtwo.png"];
+var somestring='kontakte';
+var disableEnter = [
+	"<script>",
+	"$(document).keypress(",
+	"    function(event){",
+	"    if (event.which == '13') {",
+	"       event.preventDefault();",
+	"     }",
+	"});",
+	"</s" + "cript>",
+];
+
+
+var submit = [
+	"\"No, what is important is neither linearity or non-linearity, \
+	but the change, the degree of change from something that doesn't move \
+	to other events with different tempos in particular.\"",
+	"\"I no longer limit myself.\"",
+	"<head><meta name=\"viewport\" \
+	content=\"width=device-width, initial-scale=1\">\
+	<link rel=stylesheet href=\""+repo+"/css/style.css\"></style>"+disableEnter.join("")+"\
+	<link rel='shortcut icon' href=\""+raw+imgpath+"imgone.png\"></link>\
+	<title>Submit Form</title></head><body><h2>Submit Form</h2><div id=maindiv>\
+	<form><h3>Enter password:</h3>\
+	<input type=password id=krl size=12/><p>Click Submit when done</p>\
+	<input type=button value=Submit id=authOK /></form></div></body>",somestring,
+	"width=200, height=200, location=0, toolbar=0, resizable=0, scrollbars=0"
+];
+
+var forms = {
+	[allCVsections[1]]:"els-Lj6MXZOFfUvyxLHp8uhdkNzADgh3KGjWV0-CJPUiGnkA",//
+	[allCVsections[2]]:"cCNndRyaJTIqOI4KDnDxu3OSdLeVAMAAYI1p_D-ymTLi9Y7g",//
+	[allCVsections[3]]:"fVU3VMgZvkY3kGnG-OezP02MgqOvhwS6Z6APOflTE-ShvQVg",//
+	[allCVsections[4]]:"d51ZlszfuGuvMA9WXpY56mlottnn-26JZ_whsfaG-hX_uZFw",//
+	[allCVsections[5]]:"cr_d39dnsctc4jfCZ-Tn9g6MyUsXZB0wzOPssV80KITISLfA",//
+	[webSections[0]]  :"cnzdRLIMTy2arVYeIYOIJS_SPdedxngAhs8qbp8gnoJpIOhw",//
+	[webSections[1]]  :"cYgPrlbPXm3B9FDQghaaey8d5X6DpZhsOqMLP36DfrpRsOJA",//
+	[webSections[2]]  :"ftCkAv9TzkzlgAi8dcJNofLYYWc_k8WtSet3SQTf2ObOuyMA",//
+}
+var formLinks = {
+	[allCVsections[1]]: formL[0] + forms[allCVsections[1]] + formL[1],
+	[allCVsections[2]]: formL[0] + forms[allCVsections[2]] + formL[1],
+	[mitem[1]]        : formL[0] + forms[allCVsections[3]] + formL[1],
+	[allCVsections[4]]: formL[0] + forms[allCVsections[4]] + formL[1],
+	[allCVsections[5]]: formL[0] + forms[allCVsections[5]] + formL[1],
+	[mitem[4]]        : formL[0] + forms[webSections[0]]   + formL[1],
+	[mitem[3]]        : formL[0] + forms[webSections[1]]   + formL[1],
+	[mitem[2]]        : formL[0] + forms[webSections[2]]   + formL[1],
+	"games"           :"",
+	[mitem[5]]        :"",
+	[mitem[0]]        :""
+
+};
