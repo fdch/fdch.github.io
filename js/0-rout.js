f38bf1274080d0ab367285635bf80741fe14d69c
Fede Camara Halac <camarafede@gmail.com>
2019-06-27 13:27:24 +0200

    cleanup

--- js/0-rout.js
+++ js/0-rout.js
@@ -3,8 +3,8 @@ function removeChilds(x)
   while (x.firstChild) {
       x.removeChild(x.firstChild);
   } 
-  if(!x.firstChild) return 1
-};
+  if(!x.firstChild) return 1;
+}
 
 function toggle(id)
 {
@@ -14,12 +14,12 @@ function toggle(id)
   } else {
       x.style.display = "none";
   }
-};
+}
 
 function makeID(x)
 {
   return x.replace(/ /g,'_').toLowerCase().slice(0,7);
-};
+}
 
 function unique(array)
 {
@@ -28,7 +28,7 @@ function unique(array)
   {
     return index == array.indexOf(el);                  
   });
-};
+}
 
 function shuffleArray(array)
 {
@@ -42,7 +42,7 @@ function shuffleArray(array)
       array[j] = temp;
   }
   return array;
-};
+}
 
 function width()
 {
@@ -50,7 +50,7 @@ function width()
       || document.documentElement.clientWidth 
       || document.body.clientWidth 
       || 0;
-};
+}
 
 function height()
 {
@@ -58,7 +58,7 @@ function height()
       || document.documentElement.clientHeight 
       || document.body.clientHeight 
       || 0;
-};
+}
 
 function articleWidth(maxW)
 {
@@ -66,10 +66,11 @@ function articleWidth(maxW)
   if (winW >= maxW)
   {
     return maxW * widthFactor;
-  } else {
+  } else
+  {
     return winW * widthFactor;
   }
-};
+}
 
 function resized()
 {
@@ -95,7 +96,7 @@ function resized()
   }
 
   if(mainTag)   mainTag.style.marginTop = hH+"px";
-};
+}
 
 function loadJSON(x,callback)
 {
@@ -108,7 +109,7 @@ function loadJSON(x,callback)
     }
   };
   xobj.send(null);  
-};
+}
 
 // function randomVideo()
 // {
@@ -120,7 +121,7 @@ function pdRandom(range,offset)
 {
   offset = offset || 0;
   return Math.floor(Math.random() * range) + offset;
-};
+}
 
 function randomColor(preset, target)
 {
@@ -133,13 +134,13 @@ function randomColor(preset, target)
   var menuy = "rgba("+pdRandom(dran,doff)+","+pdRandom(dran,doff)+","+pdRandom(dran,doff)+","+hAlpha+")";
   target[0].style.backgroundColor = darky;
   target[0].style.color = light;
-};
+}
 
 function navClicker()
 {
   // used in navigation tag
   randomColor(color_preset['high'],[bodyTag,headerTag[0]]);
-};
+}
 
 
 function makeCateg(x)
@@ -161,7 +162,7 @@ function makeCateg(x)
   }
   // console.log(cats.join(tilde));
   return cats;
-};
+}
 
 
 
@@ -173,37 +174,42 @@ function getUniqueCategories(x)
     cats.push(st.replace(/\W/g,' '));
   }
   return unique(unique(cats));
-};
+}
 
 function getValue(x)
 {
   // used in dropdowns as onclick attribute
   var name = x.name;
   var valu = x.value;
-  var onoff;
-  for (let i in allWorkId) {
-    let wid = document.getElementById(allWorkId[i]);
-    switch(name) {
-    case "title":
-      if(valu === wid)
-        wid.style.display = "initial";
-      else
-        wid.style.display = "none";
-      break;
-    case "category":
-      let clist = wid.classList;
-      // console.log(clist);
-      if( clist.contains(valu || valu.replace(/W/g,'')) )
-        wid.style.display = "initial";
-      else
-        wid.style.display = "none";
-      break;
-    default:
-      break;
+  if (allWorkId.length)
+  {
+    for (let i in allWorkId) {
+      let wid = document.getElementById(allWorkId[i]);
+      switch(name) {
+      case "title":
+        if(valu === wid)
+          wid.style.display = "initial";
+        else
+          wid.style.display = "none";
+        break;
+      case "category":
+        let clist = wid.classList;
+        // console.log(clist);
+        if( clist.contains(valu || valu.replace(/W/g,'')) )
+          wid.style.display = "initial";
+        else
+          wid.style.display = "none";
+        break;
+      default:
+        break;
+      }
     }
+  } else
+  {
+    console.log("allWorkId is empty:", allWorkId);
   }
   //console.log(name + ": " + valu);
-};
+}
 
 
 
@@ -225,8 +231,8 @@ function getSubmit(target)
       n.moveBy(articleWidth(maxWidth)*0.3, 0);
       open(page, "asdfyj");
     }
-  }
-};
+  };
+}
 
 function menuClick(x,e)
 {
@@ -237,7 +243,7 @@ function menuClick(x,e)
   } else {
     display(id);
   }
-};
+}
 
 function menuTouchStart(x,e)
 {
@@ -248,7 +254,7 @@ function menuTouchStart(x,e)
   } else {
     display(id);
   }
-;
+}
 
 function anchor(link,text,target)
 {
@@ -262,7 +268,7 @@ function anchor(link,text,target)
   tag.appendChild(document.createTextNode(text?text:''));
 
   return tag;
-};
+}
 
 function element(tag,text,id,onclick,width)
 {
@@ -285,7 +291,7 @@ function element(tag,text,id,onclick,width)
   
   elem.appendChild(node);
   return elem;
-};
+}
 
 function img(src,width,titl,id,tURL)
 {
@@ -306,14 +312,14 @@ function img(src,width,titl,id,tURL)
   anc.appendChild(tag);
   div.appendChild(anc);
   return div;      
-};
+}
 
 function mobileCheck()
 {
   var check = false;
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
   return check;
-};
+}
 
 // var whichone = 0;
 // var flipped = 1;
